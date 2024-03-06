a88d3e43bf9c41674b614a500df282aafd31d863
simsim <simeon.campbell@live.com>
2024-03-06 13:16:49 +0700

    addElection func tests

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -40,6 +40,7 @@ contract VotingContract {
     struct Election {
         uint electionId;
         string[] candidates;
+        uint electionStartTime;
         uint electionEndTime;
         ElectionStatus status;
     }
@@ -55,13 +56,25 @@ contract VotingContract {
     }
 
     /* Functions */
-    // Function to add a new election which takes a list of candidates and the chosen length of time for the election as input
+    /**
+     * @dev Function to add an election to the contract
+     * @param _candidates The list of candidates for the election
+     * @param _timeDays The number of days the election will be open
+     */
     function addElection(string[] memory _candidates, uint _timeDays) public onlyOwner {
+        // Require that the election has at least 2 candidates
+        require(_candidates.length > 1, "There must be more than one candidate");
+        // Maximum amount of candidates in 5
+        require (_candidates.length <= 5, "An election can have a maximum of 5 candidates");
+
+        // Store the start time of the election
+        uint electionStartTime = block.timestamp;
+
         // Calculate the end time of the election
         uint electionEndTime = block.timestamp + (_timeDays * 1 days);
 
         // Add the election to the list of elections
-        Election memory newElection = Election(electionCount, _candidates, electionEndTime, ElectionStatus.open);
+        Election memory newElection = Election(electionCount, _candidates, electionStartTime, electionEndTime, ElectionStatus.open);
 
         // Record the election in the mapping
         elections[electionCount] = newElection;
@@ -74,6 +87,19 @@ contract VotingContract {
     }
 
     // Function to close an election when the electionEndTime has passed
+    function closeElection(uint _electionId) public {
+        // Require that the election exists
+        require(_electionId > 0 && _electionId < electionCount, "Election does not exist");
+
+        // Get the election
+        Election storage election = elections[_electionId];
+
+        // Check if the current time is past the election end time
+        if (block.timestamp > election.electionEndTime) {
+            // If it is, set the election status to closed
+            election.status = ElectionStatus.closed;
+        }
+    }  
 
     /* Getter Functions */
     // Function to get the owner of the contract
@@ -87,8 +113,8 @@ contract VotingContract {
     }
 
     // Function to get the details of an election
-    function getElection(uint _electionId) public view returns (string[] memory, uint, ElectionStatus) {
-        return (elections[_electionId].candidates, elections[_electionId].electionEndTime, elections[_electionId].status);
+    function getElection(uint _electionId) public view returns (string[] memory, uint, uint, ElectionStatus) {
+        return (elections[_electionId].candidates, elections[_electionId].electionStartTime , elections[_electionId].electionEndTime, elections[_electionId].status);
     }
 
     // Function to get the candidates of an election
@@ -101,8 +127,16 @@ contract VotingContract {
         return elections[_electionId].status;
     }
 
+    // Function to get the start time of an election
+    function getElectionStartTime(uint _electionId) public view returns (uint) {
+        return elections[_electionId].electionStartTime;
+    }
+
     // Function to get the end time of an election
     function getElectionEndTime(uint _electionId) public view returns (uint) {
         return elections[_electionId].electionEndTime;
     }
 }
+
+
+
--- test/RCVotingSystem.test.js
+++ test/RCVotingSystem.test.js
@@ -66,17 +66,100 @@ describe("BallotContract", function () {
 })
 
 describe("VotingContract", function () {
-    let VotingContract, votingContract, owner, addr1, addr2, addr3, addr4, vote
+    let VotingContract,
+        votingContract,
+        owner,
+        addr1,
+        candidatesList,
+        candidate1,
+        candidate2,
+        candidate3,
+        electionTimeLimit,
+        electionStartTime,
+        electionEndTime
 
     beforeEach(async function () {
         VotingContract = await ethers.getContractFactory("VotingContract")
-        ;[owner, addr1, addr2, addr3, addr4] = await ethers.getSigners()
+        ;[owner, addr1] = await ethers.getSigners()
         votingContract = await VotingContract.deploy()
     })
 
     describe("Deployment", function () {
         it("Should set the right owner", async function () {
             expect(await votingContract.getOwner()).to.equal(owner.address)
+        })
+    })
+
+    describe("addElection", function () {
+        beforeEach(async function () {
+            candidate1 = "Candidate 1"
+            candidate2 = "Candidate 2"
+            candidate3 = "Candidate 3"
+
+            electionTimeLimit = 1
+
+            await votingContract.addElection(
+                [candidate1, candidate2, candidate3],
+                electionTimeLimit,
+            )
+
+            candidatesList = await votingContract.getElectionCandidates(1)
+            electionStatus = await votingContract.getElectionStatus(1)
+            electionStartTime = await votingContract.getElectionStartTime(1)
+            electionEndTime = await votingContract.getElectionEndTime(1)
+        })
+        it("Reverts is there is only one candidate", async function () {
+            await expect(
+                votingContract.addElection(["only1Candidate"], 1),
+            ).to.be.revertedWith("There must be more than one candidate")
+        })
+
+        it("Reverts if there are more than 5 candidates", async function () {
+            await expect(
+                votingContract.addElection(
+                    [
+                        "Candidate1/6",
+                        "Candidate2/6",
+                        "Candidate3/6",
+                        "Candidate4/6",
+                        "Candidate5/6",
+                        "Candidate6/6",
+                    ],
+                    electionTimeLimit,
+                ),
+            ).to.be.revertedWith(
+                "An election can have a maximum of 5 candidates",
+            )
+        })
+
+        it("Succesfully creates and maps an election", async function () {
+            expect(await votingContract.getElection(1)).to.deep.equal([
+                candidatesList,
+                electionStartTime,
+                electionEndTime,
+                electionStatus,
+            ])
+        })
+
+        it("Should incriment the Election count after each election", async function () {
+            const electionCount = await votingContract.getElectionCount()
+            const counterIncriment = ethers.parseUnits("1", 0)
+            await votingContract.addElection(
+                ["Candidate 1", "Candidate 2", "Candidate 3"],
+                electionTimeLimit,
+            )
+            expect(await votingContract.getElectionCount()).to.equal(
+                electionCount + counterIncriment,
+            )
+        })
+
+        it("Should emit an ElectionCreated event", async function () {
+            await expect(
+                votingContract.addElection(
+                    ["Candidate 1", "Candidate 2", "Candidate 3"],
+                    electionTimeLimit,
+                ),
+            ).to.emit(votingContract, "ElectionCreated")
         })
     })
 })
