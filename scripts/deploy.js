34f74aabbd7129d28ea8e1a55492763e55849b54
simsim <simeon.campbell@live.com>
2024-03-07 14:34:08 +0700

    deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,35 +4,68 @@ const { developmentChains } = require("../helper-hardhat-config")
 
 async function main() {
     if (developmentChains.includes(network.name)) {
-        const OrderSystem = await hre.ethers.getContractFactory("OrderSystem")
+        // Deploy the VotingContract
+        const VotingContract =
+            await hre.ethers.getContractFactory("VotingContract")
 
-        console.log("Deploying...")
+        console.log("Deploying VotingContract...")
 
-        const orderSystem = await OrderSystem.deploy()
-        console.log(`OrderSystem deployed to: ${orderSystem.target}`)
+        const votingContract = await VotingContract.deploy()
+        console.log(`VotingContract deployed to: ${votingContract.target}`)
+
+        // Deploy the BallotContract
+        const BallotContract =
+            await hre.ethers.getContractFactory("BallotContract")
+
+        console.log("Deploying BallotContract...")
+
+        const ballotContract = await BallotContract.deploy()
+        console.log(`BallotContract deployed to: ${ballotContract.target}`)
     }
 
     if (
         !developmentChains.includes(network.name) &&
         process.env.ETHERSCAN_API_KEY
     ) {
-        const OrderSystem = await hre.ethers.getContractFactory("OrderSystem")
+        // Deploy the VotingContract
+        const VotingContract =
+            await hre.ethers.getContractFactory("VotingContract")
 
-        console.log("Deploying...")
+        console.log("Deploying VotingContract...")
 
-        const orderSystem = await OrderSystem.deploy()
-        console.log(`OrderSystem deployed to: ${orderSystem.target}`)
+        const votingContract = await VotingContract.deploy()
+        console.log(`VotingContract deployed to: ${votingContract.target}`)
 
         const desiredConfirmations = 6
-        const receipt = await orderSystem
+        const votingReceipt = await votingContract
             .deploymentTransaction()
             .wait(desiredConfirmations)
 
         console.log(
-            `Transaction confirmed. Block number: ${receipt.blockNumber}`,
+            `Transaction confirmed. Block number: ${votingReceipt.blockNumber}`,
         )
-        await hre.run("verify:etherscan", { address: orderSystem.target })
-        console.log("OrderSystem verified!")
+        await hre.run("verify:etherscan", { address: votingContract.target })
+        console.log("VotingContract verified!")
+        console.log("--------------------------------------------------")
+
+        // Deploy the VotingContract
+        const BallotContract =
+            await hre.ethers.getContractFactory("BallotContract")
+
+        console.log("Deploying VotingContract...")
+
+        const ballotContract = await BallotContract.deploy()
+        console.log(`BallotContract deployed to: ${ballotContract.target}`)
+
+        const ballotReceipt = await votingContract
+            .deploymentTransaction()
+            .wait(desiredConfirmations)
+
+        console.log(
+            `Transaction confirmed. Block number: ${ballotReceipt.blockNumber}`,
+        )
+        await hre.run("verify:etherscan", { address: ballotContract.target })
+        console.log("BallotContract verified!")
         console.log("--------------------------------------------------")
     }
 }
